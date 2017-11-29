4bc720a9a6e5a4b4ee5e4b8b3d7a64081645631f
Karl Xu <karl.yogi.xu@gmail.com>
2017-11-29 22:14:38 +0800

    add backup automation and cron job

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# jenkins-deployment
+# Backup Strategy
+
+1. `jenkins/` backup at midnight everyday
+
+2. remove the backup older than 7 days
+
+# Environment Variables
+
+- `JENKINS_BACKUP_PATH` `/mydata/jenkins`
+
+# Cron Jobs
+
+Sequantial order of date parts:
+
+minute (from 0 to 59) hour (from 0 to 23) day of month (from 1 to 31) month (from 1 to 12) day of week (from 0 to 6) (0=Sunday)
+
+## Scripts
+
+1. backup to external disk
+
+    `0 0 * * * /root/Workspace/jenkins-deployment/automation backup`
--- automation.sh
+++ automation.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
+
+# load environment variables
+. ./.env
+
 White='\033[1;36m'
 NC='\033[0m' # No Color
+
 
 case "$1" in
 
     "dep" )
         docker run --name myjenkins -p 8080:8080 -p 50000:50000 -u root -v $(which docker):/usr/bin/docker -v $PWD/jenkins:/var/jenkins_home -d -v /var/run/docker.sock:/var/run/docker.sock jenkins
         ;;
+    "backup" ) # backup ./jenkins content 
+        tar czf $JENKINS_BACKUP_PATH/$(date +'%Y-%b-%d-%H-%M-%S')_jenkins.tar.gz ./jenkins
+        find $JENKINS_BACKUP_PATH -mindepth 1 -mtime +7 -exec rm {} \;
+        ;;
     * )
         echo "${White}dep${NC}: deploy jenkins"
+        echo "${White}backup${NC}: backup jenkins"
 
 esac
